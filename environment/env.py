5e469c68147e38b3d6de2e208465b8b73ad8babc
landjbs <landjbs@gmail.com>
2021-02-25 14:44:29 -0800

    input dims

--- environment/env.py
+++ environment/env.py
@@ -4,6 +4,7 @@ Simple environment for driving.
 
 import gym
 import torch
+import numpy as np
 
 
 class Config:
@@ -11,21 +12,31 @@ class Config:
         # map creation
         self.map_x_len = 10
         self.map_y_len = 10
+        # vision
+        self.camera_num = 4
+        self.frame_height = 10
+        self.frame_width = 10
 
 
 class Environment(gym.Env):
-    def __init__(self, config = None):
+    def __init__(self, config):
         # top-down map
         self.top_map = self.build_map(10, 15)
         #
         # action dimensions
         # gas: [0., 1.], brake: [0., 1.], steer: [-1., 1]
-        self.action_space = gym.spaces.Box(
-            torch.tensor([0., 0., -1.]), torch.tensor([+1., +1., +1.])
-        )
+        # self.action_space = gym.spaces.Box(
+            # torch.tensor([0., 0., -1.]), torch.tensor([1., 1., 1.])
+        # )
         # observation dimensions
-        self.observation_space = gym.spaces.Discrete(
-            
+        #
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(
+                config.camera_num, config.frame_height, config.frame_width, 3
+            ),
+            dtype=np.uint8
         )
 
     def build_map(self, x_len: int, y_len: int):
@@ -62,6 +73,8 @@ class Environment(gym.Env):
 # tests
 import matplotlib.pyplot as plt
 
-e = Environment()
-plt.imshow(e.top_map)
-plt.show()
+c = Config()
+e = Environment(c)
+e.render()
+# plt.imshow(e.top_map)
+# plt.show()
